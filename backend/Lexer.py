301c8f0c87cb188acbfcbf13b09cd494536d2258
Mariano Camposeco <mariano1941@outlook.es>
2023-06-05 20:14:53 -0600

    Empezando estructura de Lexer

---
+++ backend/Lexer.py
@@ -0,0 +1,42 @@
+import ply.lex as lex
+
+keywords={
+    'null' : 'NULL',
+    'number' : 'NUMBER',
+    'boolean':'BOOLEAN',
+    'string':'STRING',
+    'any':'ANY',
+}
+symbols=(
+    'MULT',
+    'PLUS',
+    'DIV',
+    'NEG',
+    
+)
+
+t_MULT = r'\*'
+t_PLUS = r'\+'
+t_DIV = r'\/'
+t_NEG = r'\-'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+t_MULT = r'\*'
+
