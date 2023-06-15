ff3e5384c4aaeeb3ba108004cb88b9ebedf46bf7
Manuel Rojas <manuerjs70@gmail.com>
2023-06-15 17:09:03 -0600

    update: while modificado

--- backend/src/Instruction/loop_while.py
+++ backend/src/Instruction/loop_while.py
@@ -10,25 +10,33 @@ class While(Abstract):
         self.instructions = instructions
         self.line = line
         self.colum = column
-    
+
     def execute(self, tree, table):
         while True:
-            flag = False
             condition = self.condition.execute(tree, table)
-            if isinstance(condition, CompilerException): return condition
-            if self.condition.type == 'boolean':
-                if bool(condition) == True:
-                    for instruction in self.instructions:
-                        scope = SymbolTable(table)
-                        value = instruction.execute(tree, scope)
-                        if isinstance(value, CompilerException):
-                            tree.setExceptions(value)
-                            flag = True
-                            break
-                else: 
-                    break
-            else:
+
+            if isinstance(condition, CompilerException):
+                return condition
+
+            if self.condition.type != 'boolean':
                 return CompilerException("Semantico", "Condicion no valida en el While", self.line, self.column)
-            if flag:
-                continue
-            
+
+            if bool(condition):
+                for instruction in self.instructions:
+                    scope = SymbolTable(table)
+                    value = instruction.execute(tree, scope)
+
+                    if isinstance(value, CompilerException):
+                        tree.setExceptions(value)
+                        return value
+
+                    # if isinstance(value, Break):
+                    #     return None
+
+                    # if isinstance(value, Return):
+                    #     return value
+
+                    # if isinstance(value, Continue):
+                    #     break
+            else:
+                break
